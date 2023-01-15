b19fa4f9f3503d6da19d387d28b731b5f1952445
Arjun <arjunniranjanemail@gmail.com>
2023-01-15 12:35:28 +0000

    commit

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from dash import Dash, html, dcc
-from dash_bootstrap_components.themes import BOOTSTRAP
+from dash_bootstrap_components.themes import CYBORG
 import plotly.express as px
 import pandas as pd
 
 def main() -> None:
-    app = Dash(external_stylesheets=[BOOTSTRAP])
+    app = Dash(external_stylesheets=[CYBORG])
+    
     app.title = "eCommerce sales analysis dashboard"
-    app.layout = html.Div(children=[html.H1(children="eCommerce Business Transactions analysis dashboard"),html.Hr()])
+    app.layout = html.Div(children=[html.H2(children="eCommerce Business Transactions analysis dashboard"),html.Hr()])
+    
     app.run()
 
 if __name__ == "__main__":
